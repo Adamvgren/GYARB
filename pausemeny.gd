fbbde95069b7c6f23e552f50bfa2cd769a92e23b
adamvgren <adam.vestergren@gmail.com>
2026-01-27 16:01:22 +0100

    Gjort klart main meny och 80% klar med pause meny

---
+++ pausemeny.gd
@@ -0,0 +1,29 @@
+extends Control
+
+
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("Blur")
+func paus():
+	get_tree().paused = true
+	$AnimationPlayer.play("Blur")
+
+func testESC():
+	if Input.is_action_just_pressed("Escape") and get_tree().paused == false:
+		paus()
+	elif Input.is_action_just_pressed("Escape") and get_tree().paused == true:
+		resume()
+
+
+func _on_resume_pressed() -> void:
+	resume()
+
+
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _process(delta):
+	testESC()
